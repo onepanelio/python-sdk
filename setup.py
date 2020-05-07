196cb14123c18efffc8278627a710631a56b60b9
Aleksandr Melnikov <aleksandr@onepanel.io>
2020-05-07 16:34:34 -0700

    Added Makefile for pushing python sdk to pypi.

--- setup.py
+++ setup.py
@@ -12,8 +12,8 @@
 
 from setuptools import setup, find_packages  # noqa: H301
 
-NAME = "onepanel.core.api"
-VERSION = "1.0.0-beta1"
+NAME = "onepanel-core-sdk"
+VERSION = "1.0.0-b1"
 # To install the library, run the following
 #
 # python setup.py install
