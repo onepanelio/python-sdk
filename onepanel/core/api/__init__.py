f478775305dbe9e91e6123a6b351db54cd6e551e
Andrey Melnikov <vafilor@gmail.com>
2020-10-22 11:34:05 -0700

    fix: Package version to have v prefix.

--- onepanel/core/api/__init__.py
+++ onepanel/core/api/__init__.py
@@ -14,7 +14,7 @@
 
 from __future__ import absolute_import
 
-__version__ = "0.14.0"
+__version__ = "v0.14.0"
 
 # import apis into sdk package
 from onepanel.core.api.api.auth_service_api import AuthServiceApi
