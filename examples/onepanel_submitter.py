a6008f460231a2481710b44795e83eb6d10c1ecd
Inoh Francis Monton <37641302+inohmonton99@users.noreply.github.com>
2020-11-06 06:11:51 +0800

    Update onepanel_submitter.py
    applied changes from Andrey's updates

--- examples/onepanel_submitter.py
+++ examples/onepanel_submitter.py
@@ -13,7 +13,6 @@ from onepanel.core.api.rest import ApiException
 class OnepanelSubmitter(ArgoSubmitter):
     """A submitter which submits a workflow to Onepanel"""
 
-    namespace = os.getenv('ONEPANEL_RESOURCE_NAMESPACE')
     # username = 'admin'
     # host = os.getenv('ONEPANEL_API_URL')
 
@@ -21,6 +20,7 @@ class OnepanelSubmitter(ArgoSubmitter):
         # NOTE: Need to take username, api_url here as well
         # If token and api_url are not passed in, use the values from token file and environment variable
         # If those don't exist, throw an exception
+        self.namespace = os.getenv('ONEPANEL_RESOURCE_NAMESPACE')
         self.token = token
         self.workflow_name = workflow_name
         self.host = host
@@ -34,52 +34,61 @@ class OnepanelSubmitter(ArgoSubmitter):
             self.configuration.api_key['authorization'] = api_token
             self.configuration.api_key_prefix['authorization'] = 'Bearer'
             logging.info('Onepanel configuration detected')
-
         except Exception:
             api_token = self._get_token(host, username, token)
             self.configuration.api_key['authorization'] = api_token
             self.configuration.api_key_prefix['authorization'] = 'Bearer'
-        except Exception as e:
-            print(e)
         with onepanel.core.api.ApiClient(self.configuration) as api_client:
             # Create an instance of the API class
             self.api_instance = onepanel.core.api.WorkflowTemplateServiceApi(api_client)
         logging.info('Initialized')
 
-    # If template exist, compare the crc32 hash of workflow_yaml and the YAML returned from get_workflow_template()
-    def _get_workflow_template(self, namespace, name, workflow_template):
+    def _get_workflow_template(self, namespace, name):
+        """
+        Attempts to get the workflow template from the API.
+        If the template exists, it is returned.
+        If it does not, None is returned.
+        """
         try:
             with onepanel.core.api.ApiClient(self.configuration) as api_client:
                 # Create an instance of the API class
                 api_instance = onepanel.core.api.WorkflowTemplateServiceApi(api_client)
                 api_response = api_instance.get_workflow_template(namespace, name)
-                yaml_str = api_response.to_dict()['manifest']
-                hash1 = str(zlib.crc32(workflow_template.encode()))
-                hash2 = str(zlib.crc32(yaml_str.encode()))
-            return bool(hash1 != hash2)
+                return api_response
         except ApiException as e:
-            return
+            if e.status == 404:
+                return None
+        return None
+
+    def _are_workflow_template_manifests_equal(self, workflow_template, manifest):
+        """
+        Compare the crc32 hash of the workflow_template manifest and the input manifest
+        If they are the same (same contents) return true, false otherwise.
+
+        If workflow_template is None, false is automatically returned.
+        """
+        if workflow_template is None:
+            return false
+        yaml_str = workflow_template.to_dict()['manifest']
+        hash1 = str(zlib.crc32(manifest.encode()))
+        hash2 = str(zlib.crc32(yaml_str.encode()))
+        return hash1 == hash2
 
     def _create_workflow_template(self, namespace, body):
-        try:
-            workflow_template = self.api_instance.create_workflow_template(
+        with onepanel.core.api.ApiClient(self.configuration) as api_client:
+            api_instance = onepanel.core.api.WorkflowTemplateServiceApi(api_client)
+            workflow_template = api_instance.create_workflow_template(
                 namespace,
                 body
             )
-            self._execute_workflow(namespace, workflow_template.uid)
-        except ApiException as e:
-            print("Exception when calling WorkflowTemplateServiceApi->create_workflow_template: %s\n" % e)
+            logging.info("New template created")
+            return workflow_template
 
     def _create_template_version(self, namespace, workflow_template_uid, body):
         with onepanel.core.api.ApiClient(self.configuration) as api_client:
-            # Create an instance of the API class
             api_instance=onepanel.core.api.WorkflowTemplateServiceApi(api_client)
-            try:
-                api_instance.create_workflow_template_version(namespace, workflow_template_uid, body)
-                logging.info("New template version added to %s" % workflow_template_uid)
-                self._execute_workflow(namespace, workflow_template_uid)
-            except ApiException as e:
-                print("Exception when calling WorkflowTemplateServiceApi->create_workflow_template_version: %s\n" % e)
+            api_instance.create_workflow_template_version(namespace, workflow_template_uid, body)
+            logging.info("New template version added to %s" % workflow_template_uid)
 
     def _execute_workflow(self, namespace, workflow_template_uid):
         params = []
@@ -92,12 +101,8 @@ class OnepanelSubmitter(ArgoSubmitter):
                 parameters=params,
                 workflow_template_uid=workflow_template_uid,
                 labels=[{'key': 'created-by', 'value': 'couler'}])
-            try:
-                workflow = api_instance.create_workflow_execution(namespace, body)
-                if workflow:
-                    logging.info('Executing Workflow %s in namespace: %s' % (workflow.name, self.namespace))
-            except ApiException as e:
-                print('There was an error executing the Workflow: %s\n' % e)
+
+            return api_instance.create_workflow_execution(namespace, body)
 
     def _get_token(self, host, username, token):
         with onepanel.core.api.ApiClient(self.configuration) as api_client:
@@ -119,24 +124,33 @@ class OnepanelSubmitter(ArgoSubmitter):
         return manifest
 
     def submit(self, workflow_yaml, secrets=None):
-        if secrets:
-            for secret in secrets:
-                self._create_secret(secret.to_yaml())
+        if secrets is None:
+            secrets = []
+
+        for secret in secrets:
+            self._create_secret(secret.to_yaml())
+
         if len(self.workflow_name) >= 20:
             logging.info('Name must be 20 characters or less.')
             return
+
         manifest = self.template_to_argo(workflow_yaml)
         # NOTE: `label` was added here
         body = onepanel.core.api.WorkflowTemplate(
             manifest=manifest,
             name=self.workflow_name,
-            labels=[{'key': 'created-by', 'value': 'couler'}])
-        # The method calls should all be here as follows:
-        # _get_workflow_template
-        # If workflow_template exists, call _create_worfklow_template_version
-        # If it does not exist, call create_workflow_template
-        workflow_template = self._get_workflow_template(self.namespace, self.workflow_name, manifest)
-        if workflow_template:
+            labels=[{'key': 'created-by', 'value': 'couler'}]
+        )
+
+        workflow_template = self._get_workflow_template(self.namespace, self.workflow_name)
+
+        # If we don't have a template, create one
+        if workflow_template is None:
+            workflow_template = self._create_workflow_template(self.namespace, body)
+        elif not self._are_workflow_template_manifests_equal(workflow_template, manifest):
+            # The contents are different, so create a new version.
+            # If the contents are the same, no need for a new version.
             self._create_template_version(self.namespace, self.workflow_name, body)
-        else:
-            self._create_workflow_template(self.namespace, body)
+
+        self._execute_workflow(self.namespace, workflow_template.uid)
+        logging.info('Workflow Executed')
