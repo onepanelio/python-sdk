8ab11b68c09d102d5cfa0f1aa8570e09a0c177ba
Inoh Francis Monton <37641302+inohmonton99@users.noreply.github.com>
2020-11-06 05:00:53 +0800

    Update onepanel_submitter.py
    updated code to match requirement when submit function is called

--- examples/onepanel_submitter.py
+++ examples/onepanel_submitter.py
@@ -48,31 +48,14 @@ class OnepanelSubmitter(ArgoSubmitter):
 
     # If template exist, compare the crc32 hash of workflow_yaml and the YAML returned from get_workflow_template()
     def _get_workflow_template(self, namespace, name, workflow_template):
-        if len(name) >= 20:
-            logging.info('Name must be 20 characters or less.')
-            return
-        manifest = self.template_to_argo(workflow_template)
-        # NOTE: `label` was added here
-        body = onepanel.core.api.WorkflowTemplate(
-            manifest=manifest,
-            name=name,
-            labels=[{'key': 'created-by', 'value': 'couler'}])
-        try:
-            with onepanel.core.api.ApiClient(self.configuration) as api_client:
-                # Create an instance of the API class
-                api_instance = onepanel.core.api.WorkflowTemplateServiceApi(api_client)
-                api_response = api_instance.get_workflow_template(namespace, name)
-                yaml_str = api_response.to_dict()['manifest']
-                hash1 = str(zlib.crc32(manifest.encode()))
-                hash2 = str(zlib.crc32(yaml_str.encode()))
-                if hash1 != hash2:
-                    self._create_template_version(namespace, name, body)
-                else:
-                    logging.info("Duplicate template, exiting...")
-        except Exception as e:
-            print(e)
-            self._create_workflow_template(namespace, body)
-
+        with onepanel.core.api.ApiClient(self.configuration) as api_client:
+            # Create an instance of the API class
+            api_instance = onepanel.core.api.WorkflowTemplateServiceApi(api_client)
+            api_response = api_instance.get_workflow_template(namespace, name)
+            yaml_str = api_response.to_dict()['manifest']
+            hash1 = str(zlib.crc32(workflow_template.encode()))
+            hash2 = str(zlib.crc32(yaml_str.encode()))
+        return bool(hash1 != hash2)
 
     def _create_workflow_template(self, namespace, body):
         try:
@@ -136,8 +119,21 @@ class OnepanelSubmitter(ArgoSubmitter):
         if secrets:
             for secret in secrets:
                 self._create_secret(secret.to_yaml())
+        if len(self.workflow_name) >= 20:
+            logging.info('Name must be 20 characters or less.')
+            return
+        manifest = self.template_to_argo(workflow_yaml)
+        # NOTE: `label` was added here
+        body = onepanel.core.api.WorkflowTemplate(
+            manifest=manifest,
+            name=self.workflow_name,
+            labels=[{'key': 'created-by', 'value': 'couler'}])
         # The method calls should all be here as follows:
         # _get_workflow_template
         # If workflow_template exists, call _create_worfklow_template_version
         # If it does not exist, call create_workflow_template
-        workflow_template=self._get_workflow_template(self.namespace, self.workflow_name, workflow_yaml)
+        workflow_template = self._get_workflow_template(self.namespace, self.workflow_name, manifest)
+        if workflow_template:
+            self._create_template_version(self.namespace, self.workflow_name, body)
+        else:
+            self._create_workflow_template(self.namespace, body)
