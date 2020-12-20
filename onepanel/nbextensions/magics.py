59c4f401b0bf1a9c5b857ecef8230849a14b1c45
rushtehrani <r@onepanel.io>
2020-12-20 14:50:28 -0800

    support local nni ui too

--- onepanel/nbextensions/magics.py
+++ onepanel/nbextensions/magics.py
@@ -8,8 +8,13 @@ class IPythonMagics(Magics):
 
     @line_magic
     def nni_ui(self, line):
-        src = '//{uid}--{namespace}.{domain}/nni'.format(
-            uid=os.getenv('ONEPANEL_RESOURCE_UID'),
-            namespace=os.getenv('ONEPANEL_RESOURCE_NAMESPACE'),
-            domain=os.getenv('ONEPANEL_DOMAIN'))
+        domain = os.getenv('ONEPANEL_DOMAIN', 'localhost')
+
+        if domain == 'localhost':
+            src = '//{domain}:8080'.format(domain=domain, port=port)
+        else:
+            src = '//{uid}--{namespace}.{domain}/nni'.format(
+                uid=os.getenv('ONEPANEL_RESOURCE_UID'),
+                namespace=os.getenv('ONEPANEL_RESOURCE_NAMESPACE'),
+                domain=domain)
         return IFrame(src=src, width='100%', height='500px')
